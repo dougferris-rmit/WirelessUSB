593821255ab5a9a209823837736b91051188f533
Doug Ferris <douglas.ferris@rmit.edu.au>
2013-05-13 16:49:09 +1000

    Removed needless files, updated Windows 7 config

--- Windows 7, 8.bat
+++ Windows 7, 8.bat
@@ -1,6 +1,6 @@
 echo off
 SET profilepath=%cd%\Windows7
 cd %profilepath%
-netsh wlan add profile filename="RMITWireless.xml user=current"
+netsh wlan add profile filename="Wireless Network Connection-RMIT-University.xml" user=current"
 echo Profile added.
 pause
--- windows 7 backup.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-SET profilepath=%cd%\Windows7
-netsh wlan export profile name="RMIT-University" folder="%profilepath%"
-echo Profile baacked up.
-pause
